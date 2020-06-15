6a2d88fc94b33458fd2280c1854c4185bef1fddf
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2020-06-15 11:00:49 +0800

    Create PlayerController.cs

---
+++ Server/Controllers/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Data;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class PlayerController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<Player>> Get()
+        {
+            var player = await DataContext.Players.GetPlayerById(long.Parse(User.Identity.Name));
+            player.Password = null;
+            return player;
+        }
+    }
+}
